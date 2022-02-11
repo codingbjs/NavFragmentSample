94f1190a8656e25a067fe3ab75e44c2659762c24
CDBJS <codingbjs@gmail.com>
2022-02-11 16:28:10 +0900

    MainActivity 에 OnClickListener 추가

--- app/src/main/java/com/codingbjs/navfragmentsample/MainActivity.java
+++ app/src/main/java/com/codingbjs/navfragmentsample/MainActivity.java
@@ -1,14 +1,45 @@
 package com.codingbjs.navfragmentsample;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.codingbjs.navfragmentsample.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        NavHostFragment navHostFragment =
+                (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment);
+        assert navHostFragment != null;
+        NavController navController = navHostFragment.getNavController();
+
+
+        binding.firstButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                navController.navigate(R.id.firstFragment);
+            }
+        });
+
+
+        binding.secondButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                navController.navigate(R.id.secondFragment);
+            }
+        });
+
     }
 }
